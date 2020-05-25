008839e6ba1ec54125898657c7d1f9529eb0a47f
Gabriela Garcia <51204436+Gabriela1004@users.noreply.github.com>
2020-05-24 21:06:42 -0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:planets_app/home_page.dart';
+
+
+void main(){
+  runApp(MaterialApp(
+    title: "Planets App",
+    home:HomePage()
+  ));
+}
